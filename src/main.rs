f75b0cf5cdcede7b68d4e62c7e2c9ea3c1396c4b
temportalflux <dustin.yost.t@gmail.com>
2023-01-27 15:45:38 -0500

    Refine theme toggle

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,94 @@
+use std::str::FromStr;
+
+use enumset::{EnumSet, EnumSetType};
 use gloo_storage::Storage;
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::JsCast;
-use web_sys::{HtmlSelectElement};
+use web_sys::HtmlSelectElement;
 use yew::prelude::*;
+
+#[derive(EnumSetType, Serialize, Deserialize)]
+enum Theme {
+	#[serde(rename = "dark")]
+	Dark,
+	#[serde(rename = "light")]
+	Light,
+}
+impl std::fmt::Debug for Theme {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Self::Dark => write!(f, "dark"),
+			Self::Light => write!(f, "light"),
+		}
+	}
+}
+impl std::fmt::Display for Theme {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		match self {
+			Self::Dark => write!(f, "Dark"),
+			Self::Light => write!(f, "Light"),
+		}
+	}
+}
+impl FromStr for Theme {
+	type Err = ();
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		match s.to_lowercase().as_str() {
+			"dark" => Ok(Self::Dark),
+			"light" => Ok(Self::Light),
+			_ => Err(()),
+		}
+	}
+}
 
 #[function_component]
 fn ThemeToggle() -> Html {
-	let theme = use_state(|| gloo_storage::LocalStorage::get::<String>("theme").unwrap_or_default());
+	let theme = use_state(|| gloo_storage::LocalStorage::get::<Theme>("theme").ok());
 
-	// Update the theme in storage and html when the element is reloaded
-	if theme.is_empty() {
-		gloo_storage::LocalStorage::delete("theme");
-		let _ = gloo_utils::document().document_element().unwrap().remove_attribute("data-theme");
-	}
-	else {
-		let _ = gloo_utils::document().document_element().unwrap().set_attribute("data-theme", &*theme);
-		let _ = gloo_storage::LocalStorage::set("theme", (*theme).clone());
-	}
+	// Update the theme in storage and html when the theme value has changed
+	use_effect_with_deps(
+		move |theme| {
+			log::debug!("Setting theme to {:?}", theme);
+			match theme.as_ref() {
+				Some(theme) => {
+					let _ = gloo_utils::document()
+						.document_element()
+						.unwrap()
+						.set_attribute("data-theme", &format!("{theme:?}"));
+					let _ = gloo_storage::LocalStorage::set("theme", (*theme).clone());
+				}
+				None => {
+					gloo_storage::LocalStorage::delete("theme");
+					let _ = gloo_utils::document()
+						.document_element()
+						.unwrap()
+						.remove_attribute("data-theme");
+				}
+			}
+		},
+		(*theme).clone(),
+	);
 
 	// When select element changes, set the value in the state so this element reloads.
 	let onchange = {
 		let theme = theme.clone();
 		Callback::from(move |e: Event| {
-			if let Some(select) = e.target().and_then(|t| t.dyn_into::<HtmlSelectElement>().ok()) {
-				theme.set(select.value());
+			if let Some(select) = e
+				.target()
+				.and_then(|t| t.dyn_into::<HtmlSelectElement>().ok())
+			{
+				theme.set(Theme::from_str(&select.value()).ok());
 			}
 		})
 	};
-	
+
 	html! {
 		<select class="select w-full max-w-xs" {onchange}>
-			<option value="" selected={*theme == ""}>{"Default"}</option>
-			<option value="dark" selected={*theme == "dark"}>{"Dark"}</option>
-			<option value="light" selected={*theme == "light"}>{"Light"}</option>
+			<option selected={*theme == None}>{"Default"}</option>
+			{EnumSet::<Theme>::all().into_iter().map(|value| html! {
+				<option value={format!("{value:?}")} selected={*theme == Some(value)}>{format!("{value}")}</option>
+			}).collect::<Vec<_>>()}
 		</select>
 	}
 }
