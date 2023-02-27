86161cc398ea10037764d541884f8c477b27e0da
temportalflux <dustin.yost.t@gmail.com>
2023-02-26 20:55:32 -0500

    Add damage type enum

--- src/system/dnd5e/data/action.rs
+++ src/system/dnd5e/data/action.rs
@@ -139,3 +139,64 @@ pub struct DamageRoll {
 	pub damage_type: String,
 	pub additional_bonuses: Vec<(i32, PathBuf)>,
 }
+
+#[derive(Clone, PartialEq, Debug)]
+pub enum DamageType {
+	Acid,
+	Bludgeoning,
+	Cold,
+	Fire,
+	Force,
+	Lightning,
+	Necrotic,
+	Piercing,
+	Poison,
+	Psychic,
+	Radiant,
+	Slashing,
+	Thunder,
+}
+impl DamageType {
+	pub fn display_name(&self) -> &'static str {
+		match self {
+			Self::Acid => "Acid",
+			Self::Bludgeoning => "Bludgeoning",
+			Self::Cold => "Cold",
+			Self::Fire => "Fire",
+			Self::Force => "Force",
+			Self::Lightning => "Lightning",
+			Self::Necrotic => "Necrotic",
+			Self::Piercing => "Piercing",
+			Self::Poison => "Poison",
+			Self::Psychic => "Psychic",
+			Self::Radiant => "Radiant",
+			Self::Slashing => "Slashing",
+			Self::Thunder => "Thunder",
+		}
+	}
+
+	pub fn description(&self) -> &'static str {
+		match self {
+			Self::Acid => "The corrosive spray of an adult black dragon's breath and the dissolving \
+			enzymes secreted by a black pudding deal acid damage.",
+			Self::Bludgeoning => "Blunt force attacks--hammers, falling, constriction, \
+			and the like--deal bludgeoning damage.",
+			Self::Cold => "The infernal chill radiating from an ice devil's spear and the frigid blast \
+			of a young white dragon's breath deal cold damage.",
+			Self::Fire => "Ancient red dragons breathe fire, and many spells conjure flames to deal fire damage.",
+			Self::Force => "Force is pure magical energy focused into a damaging form. \
+			Most effects that deal force damage are spells, including magic missile and spiritual weapon.",
+			Self::Lightning => "A lightning bolt spell and a blue dragon wyrmling's breath deal lightning damage.",
+			Self::Necrotic => "Necrotic damage, dealt by certain undead and a spell such \
+			as chill touch, withers matter and even the soul.",
+			Self::Piercing => "Puncturing and impaling attacks, including spears and \
+			monsters' bites, deal piercing damage.",
+			Self::Poison => "Venomous stings and the toxic gas of an adult green dragon's breath deal poison damage.",
+			Self::Psychic => "Mental abilities such as a psionic blast deal psychic damage.",
+			Self::Radiant => "Radiant damage, dealt by a cleric's flame strike spell or an angel's \
+			smiting weapon, sears the flesh like fire and overloads the spirit with power.",
+			Self::Slashing => "Swords, axes, and monsters' claws deal slashing damage.",
+			Self::Thunder => "A concussive burst of sound, such as the effect of the thunderwave spell, deals thunder damage.",
+		}
+	}
+}
