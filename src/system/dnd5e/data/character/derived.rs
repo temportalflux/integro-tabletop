bdb99013abd99e8df29fe9fba7e59c70a665b497
temportalflux <dustin.yost.t@gmail.com>
2023-03-17 17:22:58 -0400

    Sketch out computations for ability score evaluation

--- src/system/dnd5e/data/character/derived.rs
+++ src/system/dnd5e/data/character/derived.rs
@@ -13,7 +13,11 @@ use crate::{
 	},
 };
 use enum_map::{enum_map, EnumMap};
-use std::{collections::BTreeMap, path::PathBuf};
+use itertools::Itertools;
+use std::{
+	collections::{BTreeMap, HashSet},
+	path::PathBuf,
+};
 
 mod sense;
 pub use sense::*;
@@ -97,6 +101,175 @@ impl AbilityScores {
 
 	pub fn get(&self, ability: Ability) -> &AttributedValue<i32> {
 		&self.0[ability]
+	}
+}
+
+// Ability Scores
+// - have a default max of 20
+// - base value is in persistent data
+// - mutator can extend the max to some value
+// - mutator can add a bonus
+// - mutator can add a bonus with a constraint (e.g. total score <= x)
+struct AbilityScoreItem {
+	bonuses: Vec<(Bonus, PathBuf)>,
+	max_score: u32,
+	max_score_incs: Vec<(u32, PathBuf)>,
+}
+impl AbilityScoreItem {
+	fn from_score(score: u32) -> Self {
+		Self {
+			bonuses: vec![(
+				Bonus {
+					value: score,
+					max_total: None,
+				},
+				"Base Score".into(),
+			)],
+			max_score: 20,
+			max_score_incs: vec![],
+		}
+	}
+
+	/// Returns the evaluated total scoree, and the list of paths that were used from the list of bonuses.
+	fn evaluate(&self) -> (u32, Vec<&std::path::Path>) {
+		let max_value = self.max_score + self.max_score_incs.iter().map(|(v, _)| v).sum::<u32>();
+		let (no_constraints, constrained): (Vec<_>, Vec<_>) = self
+			.bonuses
+			.iter()
+			.enumerate()
+			.partition_map(|(idx, (bonus, _))| match bonus.max_total {
+				None => itertools::Either::Left((idx, bonus.value)),
+				Some(max_total) => itertools::Either::Right((idx, (bonus.value, max_total))),
+			});
+		let (mut used_indices, unconstrained): (HashSet<_>, Vec<_>) =
+			no_constraints.into_iter().unzip();
+		let total = unconstrained.into_iter().sum::<u32>();
+		let (total, additional_indices) = optimize_max_sums(total, constrained);
+		if let Some(indices) = additional_indices {
+			used_indices.extend(indices.into_iter());
+		}
+		let used_paths = used_indices
+			.into_iter()
+			.filter_map(|idx| self.bonuses.get(idx))
+			.map(|(_, path)| path.as_path())
+			.collect::<Vec<_>>();
+		(total.min(max_value), used_paths)
+	}
+}
+
+struct Bonus {
+	value: u32,
+	max_total: Option<u32>,
+}
+
+fn optimize_max_sums(
+	base: u32,
+	mut bonuses: Vec<(/*idx*/ usize, (/*bonus*/ u32, /*max*/ u32))>,
+) -> (u32, Option<Vec<usize>>) {
+	use itertools::Itertools;
+
+	// Lower Extents: drop all entries whose max constraint will never be met (req max < base)
+	bonuses.retain(|(_, (_, max))| *max >= base);
+
+	// The largest possible total which satisfies all constraints
+	let mut max_valid_total = base;
+	// The indices of the entries used to make the total
+	let mut best_set_indices = None;
+	// Optimization problem
+	// Find the optimal use of bonuses which maximizes the total value
+	// and where all included bonuses have a max >= the final total.
+	// NOTE: brute force - this has very poor performance
+	//println!("base={base} relevant-bonuses={bonuses:?}");
+	for size in 0..bonuses.len() {
+		for bonuses in bonuses.iter().combinations(size + 1) {
+			let (indices, bonuses): (Vec<_>, Vec<_>) = bonuses.into_iter().cloned().unzip();
+			//println!("\tCombination: {bonuses:?}");
+			let (bonuses, constraints): (Vec<_>, Vec<_>) = bonuses.into_iter().unzip();
+			let total = base + bonuses.into_iter().sum::<u32>();
+			// filter out any constrains which are met (the max >= this combination's total)
+			// any remaining are unmet constraints (max < total).
+			// if there are no remaining, then this is a valid combination.
+			let num_constraints_unmet = constraints.into_iter().filter(|max| *max < total).count();
+			//println!("\t\ttotal={total} valid? {}", num_constraints_unmet == 0);
+			if num_constraints_unmet == 0 {
+				if total > max_valid_total {
+					max_valid_total = total;
+					best_set_indices = Some(indices);
+				}
+			}
+		}
+	}
+
+	(max_valid_total, best_set_indices)
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn oms_empty() {
+		let value = optimize_max_sums(10, vec![]);
+		assert_eq!(value, (10, None));
+	}
+
+	#[test]
+	fn oms_only_lower() {
+		let value = optimize_max_sums(15, vec![(0, (1, 11)), (1, (5, 14))]);
+		assert_eq!(value, (15, None));
+	}
+
+	#[test]
+	fn oms_no_optimization() {
+		let value = optimize_max_sums(14, vec![(0, (2, 13)), (1, (4, 21))]);
+		assert_eq!(value, (18, Some(vec![1])));
+	}
+
+	#[test]
+	fn oms_no_extents() {
+		let value = optimize_max_sums(
+			9,
+			vec![
+				(0, (1, 14)),
+				(1, (2, 12)), // should drop this
+				(2, (1, 15)),
+				(3, (5, 17)),
+			],
+		);
+		assert_eq!(value, (15, Some(vec![2, 3])));
+	}
+
+	#[test]
+	fn oms_mixed() {
+		let value = optimize_max_sums(
+			8,
+			vec![
+				// lower extent (max < base)
+				(0, (2, 5)),
+				// to optimize
+				(1, (2, 15)),
+				(2, (3, 15)),
+				(3, (1, 16)),
+				(4, (3, 17)),
+				(5, (4, 18)),
+			],
+		);
+		assert_eq!(value, (16, Some(vec![3, 4, 5])));
+	}
+
+	#[test]
+	fn oms_same_max() {
+		let value = optimize_max_sums(
+			8,
+			vec![
+				(0, (1, 17)),
+				(1, (2, 17)),
+				(2, (3, 17)),
+				(3, (4, 17)),
+				(4, (5, 17)),
+			],
+		);
+		assert_eq!(value, (17, Some(vec![3, 4])));
 	}
 }
 
