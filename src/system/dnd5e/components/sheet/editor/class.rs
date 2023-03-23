3a47af711846db72a03a28377375725f573cf996
temportalflux <dustin.yost.t@gmail.com>
2023-03-22 20:08:53 -0400

    Add class level management

--- src/system/dnd5e/components/sheet/editor/class.rs
+++ src/system/dnd5e/components/sheet/editor/class.rs
@@ -136,14 +136,75 @@ fn ClassBrowser(ClassBrowserProps { on_added }: &ClassBrowserProps) -> Html {
 
 #[function_component]
 fn ActiveClassList() -> Html {
+	let system = use_context::<UseStateHandle<DnD5e>>().unwrap();
 	let state = use_context::<SharedCharacter>().unwrap();
+	let onclick_add = Callback::from({
+		let system = system.clone();
+		let state = state.clone();
+		move |idx: usize| {
+			let system = system.clone();
+			state.dispatch(Box::new(move |persistent: &mut Persistent, _| {
+				let Some(dst) = persistent.classes.get_mut(idx) else { return None; };
+				let Some(src) = system.classes.get(&dst.source_id) else { return None; };
+				let next_level_idx = dst.levels.len();
+				let Some(src_level) = src.levels.get(next_level_idx) else { return None; };
+				dst.levels.push(src_level.clone());
+				Some(ActionEffect::Recompile)
+			}));
+		}
+	});
+	let remove_class = Callback::from({
+		let state = state.clone();
+		move |idx| {
+			state.dispatch(Box::new(move |persistent: &mut Persistent, _| {
+				let _ = persistent.classes.remove(idx);
+				Some(ActionEffect::Recompile)
+			}));
+		}
+	});
+	let onclick_remove = Callback::from({
+		let state = state.clone();
+		move |idx: usize| {
+			state.dispatch(Box::new(move |persistent: &mut Persistent, _| {
+				let remove_class = {
+					let Some(class) = persistent.classes.get_mut(idx) else { return None; };
+					let _ = class.levels.pop();
+					class.levels.is_empty()
+				};
+				if remove_class {
+					let _ = persistent.classes.remove(idx);
+				}
+				Some(ActionEffect::Recompile)
+			}));
+		}
+	});
 	html! {<>
-		{state.persistent().classes.iter().map(|class| {
+		{state.persistent().classes.iter().enumerate().map(|(idx, class)| {
 			html! {
 				<div class="card my-2">
-					<div class="card-header">{class.name.clone()}</div>
+					<div class="card-header d-flex">
+						{class.name.clone()}
+						<button
+							type="button"
+							class="btn-close ms-auto" aria-label="Close"
+							onclick={remove_class.reform(move |_| idx)}
+						/>
+					</div>
 					<div class="card-body">
 						{class_body(class, true)}
+						<div class="d-flex justify-content-center">
+							<button
+								type="button" class="btn btn-success mx-2"
+								onclick={onclick_add.reform(move |_| idx)}
+							>{"Add Level"}</button>
+							<button
+								type="button" class="btn btn-danger mx-2"
+								onclick={onclick_remove.reform(move |_| idx)}
+							>{match class.levels.len() {
+								1 => "Remove Class".to_owned(),
+								_ => format!("Remove Level {}", class.levels.len()),
+							}}</button>
+						</div>
 					</div>
 				</div>
 			}
@@ -165,34 +226,39 @@ fn class_body(value: &Class, show_selectors: bool) -> Html {
 		{show_selectors.then(|| selectors_in(&value.mutators)).unwrap_or_default()}
 
 		<div class="accordion my-2" id={level_accordion_id.clone()}>
-			{value.levels.iter().enumerate().filter(|(_, level)| !level.is_empty()).map(|(idx, level)| {
+			{value.levels.iter().enumerate().filter_map(|(idx, level)| {
+				if !show_selectors && level.is_empty() {
+					return None;
+				}
 				let id = format!("{}{}", level_accordion_id, idx);
-				html! {
-					<div class="accordion-item">
-						<h2 class="accordion-header">
-							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={format!("#{id}")}>
-								{"Level "}{idx + 1}
-							</button>
-						</h2>
+				let collapse_target = format!("#{id}");
+				let body = match level.is_empty() {
+					true => html! {},
+					false => html! {
 						<div {id} class="accordion-collapse collapse" data-bs-parent={format!("#{level_accordion_id}")}>
 							<div class="accordion-body">
 								{level_body(level, show_selectors)}
 							</div>
 						</div>
+					},
+				};
+				Some(html! {
+					<div class="accordion-item">
+						<h2 class="accordion-header">
+							<button
+								class="accordion-button collapsed" type="button"
+								data-bs-toggle="collapse" data-bs-target={collapse_target}
+								disabled={level.is_empty()}
+							>
+								{"Level "}{idx + 1}
+								{level.is_empty().then_some(" - Empty").unwrap_or_default()}
+							</button>
+						</h2>
+						{body}
 					</div>
-				}
+				})
 			}).collect::<Vec<_>>()}
 		</div>
-		{show_selectors.then(|| html! {<>
-			<div class="d-flex justify-content-center">
-				<button
-					type="button" class="btn btn-success mx-2"
-				>{"Add Level"}</button>
-				<button
-					type="button" class="btn btn-danger mx-2"
-				>{format!("Remove Level {}", value.levels.len())}</button>
-			</div>
-		</>}).unwrap_or_default()}
 	</>}
 }
 
