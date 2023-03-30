a23f4d3971dc5f32277008e66b954829f95578ea
temportalflux <dustin.yost.t@gmail.com>
2023-03-30 11:36:30 -0400

    Idealize how to handle source id selectors

--- src/system/dnd5e/data/action.rs
+++ src/system/dnd5e/data/action.rs
@@ -1,9 +1,13 @@
 use super::Condition;
 use crate::{
 	kdl_ext::{DocumentExt, NodeExt, ValueIdx},
-	system::{core::NodeRegistry, dnd5e::FromKDL},
+	system::{
+		core::{NodeRegistry, SourceId},
+		dnd5e::FromKDL,
+	},
 };
-use std::path::PathBuf;
+use std::{path::PathBuf, str::FromStr};
+use anyhow::Context;
 use uuid::Uuid;
 
 mod activation;
@@ -23,7 +27,7 @@ pub struct Action {
 	/// Dictates how many times this action can be used until it is reset.
 	pub limited_uses: Option<LimitedUses>,
 	/// Conditions applied when the action is used.
-	pub apply_conditions: Vec<Condition>,
+	pub conditions_to_apply: Vec<Condition>,
 	// generated
 	pub source: Option<ActionSource>,
 }
@@ -54,6 +58,7 @@ impl FromKDL for Action {
 			&mut ValueIdx::default(),
 			node_reg,
 		)?;
+
 		let attack = match node.query("scope() > attack")? {
 			None => None,
 			Some(node) => Some(Attack::from_kdl(node, &mut ValueIdx::default(), node_reg)?),
@@ -66,8 +71,28 @@ impl FromKDL for Action {
 				node_reg,
 			)?),
 		};
-		// TODO: conditions applied on use
-		let apply_conditions = Vec::new();
+
+		let mut conditions_to_apply = Vec::new();
+		for node in node.query_all("scope() > condition")? {
+			let mut value_idx = ValueIdx::default();
+			match node.get_str_req(value_idx.next())? {
+				"Custom" => {
+					// this is a custom condition node, parse it as a condition struct
+					let condition = Condition::from_kdl(node, &mut value_idx, node_reg)?;
+					conditions_to_apply.push(condition);
+				}
+				source_id_str => {
+					let source_id = SourceId::from_str(source_id_str).with_context(|| {
+						format!("Expected {source_id_str:?} to either be the value \"Custom\" or a valid SourceId.")
+					})?;
+					// TODO: The source id may be partial (i.e. another item local to this module),
+					// need access to the ancestor's source id in order to convert to a full id.
+					// TODO: Resolve source ids after all modules have been loaded but before
+					// exiting the loading phase OR have the system on hand when applying the conditions (more likely).
+				}
+			}
+		}
+
 		Ok(Self {
 			name,
 			description,
@@ -75,7 +100,7 @@ impl FromKDL for Action {
 			activation_kind,
 			attack,
 			limited_uses,
-			apply_conditions,
+			conditions_to_apply,
 			source: None,
 		})
 	}
