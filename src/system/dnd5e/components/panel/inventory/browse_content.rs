cdcca2b8138e08ee62bca1b1681b4c4910773e9d
temportalflux <dustin.yost.t@gmail.com>
2023-04-08 10:57:17 -0400

    Limit serach time in item browser

--- src/system/dnd5e/components/panel/inventory/browse_content.rs
+++ src/system/dnd5e/components/panel/inventory/browse_content.rs
@@ -1,39 +1,149 @@
 use crate::system::{
-	core::ModuleId,
-	dnd5e::{components::{editor::CollapsableCard, panel::item_body}, data::item::Item, DnD5e},
+	core::{ModuleId, SourceId},
+	dnd5e::{
+		components::{editor::CollapsableCard, panel::item_body},
+		data::item::Item,
+		DnD5e,
+	},
 };
 use wasm_bindgen::JsCast;
 use web_sys::HtmlInputElement;
 use yew::prelude::*;
+use yew_hooks::use_async;
+
+fn now() -> f64 {
+	let window = web_sys::window().expect("missing window");
+	let perf = window.performance().expect("missing performance");
+	perf.now()
+}
 
 #[function_component]
 pub fn BrowseModal() -> Html {
+	static DEFAULT_RESULT_LIMIT: usize = 10;
+	static SEARCH_BUDGET_MS: u64 = 2;
 	let system = use_context::<UseStateHandle<DnD5e>>().unwrap();
-	let relevant_item_ids = use_state(|| Vec::new());
+
+	let search_params = use_state(|| SearchParams::default());
+	let result_limit = use_state_eq(|| Some(DEFAULT_RESULT_LIMIT));
+	let search_results = use_async({
+		let system = system.clone();
+		let search_params = search_params.clone();
+		let result_limit = result_limit.clone();
+		async move {
+			if search_params.is_empty() {
+				return Ok((Vec::<SourceId>::new(), false));
+			}
+			
+			let start = now();
+
+			let mut stopped_early = false;
+			let mut matched = Vec::with_capacity(result_limit.unwrap_or(50));
+			for (id, item) in system.items.iter() {
+				if search_params.matches(item) {
+					matched.push((id, item));
+				}
+				if let Some(max_results) = *result_limit {
+					let spent_time = now() - start;
+					if matched.len() >= max_results || spent_time >= SEARCH_BUDGET_MS as f64 {
+						stopped_early = true;
+						break;
+					}
+				}
+			}
+			
+			matched.sort_by(|(_, a), (_, b)| a.name.cmp(&b.name));
+			let ids = matched.into_iter().map(|(id, _)| id).cloned().collect::<Vec<_>>();
+			Ok((ids, stopped_early)) as Result<_, ()>
+		}
+	});
+	use_effect_with_deps(
+		{
+			let results = search_results.clone();
+			move |_params| {
+				results.run();
+			}
+		},
+		(search_params.clone(), result_limit.clone()),
+	);
 
 	let on_search_changed = Callback::from({
-		let system = system.clone();
-		let relevant_item_ids = relevant_item_ids.clone();
+		let result_limit = result_limit.clone();
+		let search_params = search_params.clone();
 		move |value: SearchParams| {
-			log::debug!(target: "inv", "Searching for: {:?}", value);
-			if value.is_empty() {
-				relevant_item_ids.set(Vec::new());
-				return;
-			}
-			let ids = system
-				.items
-				.iter()
-				.filter_map(|(id, item)| value.matches(item).then_some(id))
-				.cloned()
-				.collect::<Vec<_>>();
-			relevant_item_ids.set(ids);
+			result_limit.set(Some(DEFAULT_RESULT_LIMIT));
+			search_params.set(value);
+		}
+	});
+	let on_load_all_results = Callback::from({
+		let result_limit = result_limit.clone();
+		move |_| {
+			result_limit.set(None);
 		}
 	});
 
-	let items = {
-		let mut items = relevant_item_ids.iter().filter_map(|id| system.items.get(id).map(|item| (id, item))).collect::<Vec<_>>();
-		items.sort_by(|(_, a), (_, b)| a.name.cmp(&b.name));
-		items
+	let found_item_listings = match (search_params.is_empty(), search_results.loading, &search_results.data) {
+		(true, _, _) => html! {},
+		(_, true, _) => html! {
+			<div class="d-flex justify-content-center">
+				<div class="spinner-border" role="status">
+					<span class="visually-hidden">{"Loading..."}</span>
+				</div>
+			</div>
+		},
+		(_, false, None) => html! {
+			<div class="text-center">
+				{"No items found"}
+			</div>
+		},
+		(_, false, Some((items, stopped_early))) => html! {<>
+			{items.iter().filter_map(|id| {
+				let Some(item) = system.items.get(&id) else { return None; };
+				let card_id = format!(
+					"{}{}{}",
+					match &id.module {
+						None => String::new(),
+						Some(ModuleId::Local { name }) => format!("{name}_"),
+						Some(ModuleId::Github { user_org, repository }) => format!("{user_org}_{repository}_"),
+					},
+					{
+						let path = id.path.with_extension("");
+						path.to_str().unwrap().replace("/", "_")
+					},
+					match id.node_idx {
+						0 => String::new(),
+						idx => format!("_{idx}"),
+					}
+				);
+				Some(html! {
+					<CollapsableCard
+						id={card_id}
+						header_content={{
+							html! {<>
+								<span>{item.name.clone()}</span>
+								<button
+									type="button" class="btn btn-primary btn-xs ms-auto"
+								>
+									{"Add"}
+								</button>
+							</>}
+						}}
+					>
+						{item_body(item)}
+					</CollapsableCard>
+				})
+			}).collect::<Vec<_>>()}
+			{match (*result_limit, stopped_early) {
+				(None, _) | (Some(_), false) => html! {},
+				(Some(_), true) => html! {
+					<button
+						type="button" class="btn btn-primary"
+						onclick={on_load_all_results}
+					>
+						{"Load All Results"}
+					</button>
+				}
+			}}
+		</>},
 	};
 
 	html! {<>
@@ -44,47 +154,13 @@ pub fn BrowseModal() -> Html {
 		<div class="modal-body">
 			<SearchInput on_change={on_search_changed} />
 			<div style="height: 600px">
-				{items.into_iter().map(|(id, item)| {
-					let card_id = format!(
-						"{}{}{}",
-						match &id.module {
-							None => String::new(),
-							Some(ModuleId::Local { name }) => format!("{name}_"),
-							Some(ModuleId::Github { user_org, repository }) => format!("{user_org}_{repository}_"),
-						},
-						{
-							let path = id.path.with_extension("");
-							path.to_str().unwrap().replace("/", "_")
-						},
-						match id.node_idx {
-							0 => String::new(),
-							idx => format!("_{idx}"),
-						}
-					);
-					html! {
-						<CollapsableCard
-							id={card_id}
-							header_content={{
-								html! {<>
-									<span>{item.name.clone()}</span>
-									<button
-										type="button" class="btn btn-primary btn-xs ms-auto"
-									>
-										{"Add"}
-									</button>
-								</>}
-							}}
-						>
-							{item_body(item)}
-						</CollapsableCard>
-					}
-				}).collect::<Vec<_>>()}
+				{found_item_listings}
 			</div>
 		</div>
 	</>}
 }
 
-#[derive(Clone, Default, Debug)]
+#[derive(Clone, PartialEq, Default, Debug)]
 pub struct SearchParams {
 	text: String,
 }
--- src/system/dnd5e/components/panel/inventory/item_content.rs
+++ src/system/dnd5e/components/panel/inventory/item_content.rs
@@ -3,7 +3,11 @@ use yew::prelude::*;
 use crate::system::dnd5e::data::item::Item;
 
 pub fn item_body(item: &Item) -> Html {
-	let desc = item.description.as_ref().map(|desc| html! { <div class="text-block">{desc.clone()}</div> }).unwrap_or_default();
+	let desc = item
+		.description
+		.as_ref()
+		.map(|desc| html! { <div class="text-block">{desc.clone()}</div> })
+		.unwrap_or_default();
 	html! {<>
 		{desc}
 	</>}
