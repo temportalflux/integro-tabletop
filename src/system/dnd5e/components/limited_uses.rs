aa386ae2288fae4a6f92b97d031b571461bb8ed3
temportalflux <dustin.yost.t@gmail.com>
2023-04-21 09:36:00 -0400

    Add numerical counter for features with more than 5 uses

--- src/system/dnd5e/components/limited_uses.rs
+++ src/system/dnd5e/components/limited_uses.rs
@@ -15,16 +15,11 @@ impl<'parent> UsesCounter<'parent> {
 		let consumed_uses = self.limited_uses.get_uses_consumed(&self.state);
 		let Some(max_uses) = self.limited_uses.max_uses.evaluate(&self.state) else { return html! {} };
 
-		let toggle_use = Callback::from({
+		let consume_delta = Callback::from({
 			let state = self.state.clone();
 			let uses_path = Arc::new(self.limited_uses.get_uses_path());
-			move |evt: web_sys::Event| {
-				let Some(target) = evt.target() else { return; };
-				let Some(input) = target.dyn_ref::<HtmlInputElement>() else { return; };
-				let consume_use = input.checked();
-
-				let diff = consume_use.then_some(1).unwrap_or(-1);
-				let new_uses = (consumed_uses as i32).saturating_add(diff).max(0) as u32;
+			move |delta: i32| {
+				let new_uses = (consumed_uses as i32).saturating_add(delta).max(0) as u32;
 				let uses_path = uses_path.clone();
 				state.dispatch(Box::new(move |persistent: &mut Persistent, _| {
 					persistent.set_selected_value(uses_path.as_path(), new_uses.to_string());
@@ -33,28 +28,122 @@ impl<'parent> UsesCounter<'parent> {
 			}
 		});
 
-		let use_checkboxes = (0..max_uses)
-			.map(|idx| {
-				html! {
-					<input
-						class={"form-check-input slot"} type={"checkbox"}
-						checked={idx < consumed_uses}
-						onclick={Callback::from(|evt: web_sys::MouseEvent| evt.stop_propagation())}
-						onchange={toggle_use.clone()}
-					/>
-				}
-			})
-			.collect::<Vec<_>>();
+		let counter = match max_uses {
+			..=0 => Html::default(),
+			// we use checkboxes for anything <= 5 max uses
+			1..=5 => {
+				let toggle_use = Callback::from({
+					let consume_delta = consume_delta.clone();
+					move |evt: web_sys::Event| {
+						let Some(target) = evt.target() else { return; };
+						let Some(input) = target.dyn_ref::<HtmlInputElement>() else { return; };
+						let consume_use = input.checked();
+						consume_delta.emit(consume_use.then_some(1).unwrap_or(-1));
+					}
+				});
+
+				html! {<>
+					{(0..max_uses)
+						.map(|idx| {
+							html! {
+								<input
+									class={"form-check-input slot"} type={"checkbox"}
+									checked={idx < consumed_uses}
+									onclick={Callback::from(|evt: web_sys::MouseEvent| evt.stop_propagation())}
+									onchange={toggle_use.clone()}
+								/>
+							}
+						})
+						.collect::<Vec<_>>()}
+				</>}
+			}
+			// otherwise we use a numerical counter form
+			_ => {
+				html! {<UseCounterDelta {max_uses} {consumed_uses} on_apply={consume_delta.reform(|delta: i32| -delta)} />}
+			}
+		};
+
+		let reset_desc = self
+			.limited_uses
+			.reset_on
+			.as_ref()
+			.map(|rest| format!(" (reset on {:?} Rest)", rest))
+			.unwrap_or_default();
 
 		html! {
 			<span class="uses d-flex align-items-center">
-				<strong class="me-2">{"Uses: "}</strong>
-				{use_checkboxes}
-				{match &self.limited_uses.reset_on {
-					Some(rest) => html! { <span class="ms-2">{format!("(reset on {:?} Rest)", rest)}</span> },
-					None => html! {},
-				}}
+				<strong class="me-2">{format!("Uses{reset_desc}: ")}</strong>
+				{counter}
 			</span>
 		}
 	}
+}
+
+#[derive(Clone, PartialEq, Properties)]
+struct UseCounterDeltaProps {
+	max_uses: u32,
+	consumed_uses: u32,
+	on_apply: Callback<i32>,
+}
+
+#[function_component]
+fn UseCounterDelta(
+	UseCounterDeltaProps {
+		max_uses,
+		consumed_uses,
+		on_apply,
+	}: &UseCounterDeltaProps,
+) -> Html {
+	let delta_state = use_state_eq(|| 0);
+	
+	let clear_delta = Callback::from({
+		let delta_state = delta_state.clone();
+		move |_| {
+			delta_state.set(0);
+		}
+	});
+	
+	let apply_delta = Callback::from({
+		let delta_state = delta_state.clone();
+		let on_apply = on_apply.clone();
+		move |_| {
+			if *delta_state != 0 {
+				let delta = *delta_state;
+				delta_state.set(0);
+				on_apply.emit(delta);
+			}
+		}
+	});
+
+	let onclick_sub = Callback::from({
+		let delta_state = delta_state.clone();
+		move |_| {
+			delta_state.set(delta_state.saturating_sub(1));
+		}
+	});
+
+	let onclick_add = Callback::from({
+		let delta_state = delta_state.clone();
+		move |_| {
+			delta_state.set(delta_state.saturating_add(1));
+		}
+	});
+
+	let delta_section = match *delta_state {
+		0 => Html::default(),
+		_ => html! {<>
+			<span class="delta">{*delta_state}</span>
+			<button type="button" class="btn btn-xs btn-theme" onclick={apply_delta}>{"Apply"}</button>
+			<button type="button" class="btn btn-xs btn-theme" onclick={clear_delta}>{"Clear"}</button>
+		</>},
+	};
+
+	let uses_remaining = max_uses - consumed_uses;
+	let new_uses_remaining = (uses_remaining as i32).saturating_add(*delta_state).max(0) as u32;
+	html! {<span class="deltaform d-flex align-items-center" onclick={Callback::from(|evt: MouseEvent| evt.stop_propagation())}>
+		<button type="button" class="btn btn-theme sub" onclick={onclick_sub} disabled={new_uses_remaining == 0} />
+		<span class="amount">{format!("{new_uses_remaining} / {max_uses}")}</span>
+		<button type="button" class="btn btn-theme add" onclick={onclick_add} disabled={new_uses_remaining >= *max_uses} />
+		{delta_section}
+	</span>}
 }
